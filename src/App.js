6a17e4c0f8e32af06c2bb7bdb1638b73030e7dc6
Minseo8170 <163138618+Minseo8170@users.noreply.github.com>
2025-04-06 22:40:05 +0900

    [Feat] 최초 커밋 (마이페이지, 위키 틀)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import MyPage from './components/MyPage';
+import Wiki from './components/Wiki';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">{Wiki()}</div>;
 }
 
 export default App;
