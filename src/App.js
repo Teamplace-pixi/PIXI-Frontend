59d2c2ede8c578e8772d3cf4c31771e1b42ba477
SXXNWN <julia020115@naver.com>
2025-04-07 00:53:22 +0900

    FIXI 시작화면 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StartScreen from './StartScreen';
+import DictionaryScreen from './DictionaryScreen';
+import DictionaryScreen1 from './DictionaryScreen1';
+import DictionaryScreen2 from './DictionaryScreen2';
+import DictionaryScreen3 from './DictionaryScreen3';
+import LoginScreen from './LoginScreen';
+import StartLogin from './StartLogin';
+import SignUpScreen from './SignUpScreen';
+
 
 function App() {
-  return <StartScreen />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<StartScreen />} />
+        <Route path="/dictionary" element={<DictionaryScreen />} />
+        <Route path="/page1" element={<DictionaryScreen1 />} />
+        <Route path="/page2" element={<DictionaryScreen2 />} />
+        <Route path="/page3" element={<DictionaryScreen3 />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/startlogin" element={<StartLogin />} />
+        <Route path="/signup" element={<SignUpScreen />} /> {/* ✅ 이 줄도 포함 */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/StartScreen.jsx
+++ src/StartScreen.jsx
@@ -1,22 +1,26 @@
+// StartScreen.jsx
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './StartScreen.css';
 
-const StartScreen = () => {
+function StartScreen() {
+  const navigate = useNavigate();
+
+  const handleTouch = () => {
+    navigate('/dictionary');
+  };
+
   return (
-    <div className="start-wrapper">
+    <div className="start-wrapper" onClick={handleTouch}>
       <div className="start-container">
-        <img
-          src="/FIXIlogo.png"
-          alt="FIXI Logo"
-          className="fixi-logo"
-        />
-        <p className="start-text">
+        <img src="/FIXIlogo.png" alt="FIXI Logo" className="fixi-logo" />
+        <div className="start-text">
           소중한 나의 전자제품을<br />
           FIXI의 프리미엄 케어와 함께
-        </p>
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default StartScreen;
