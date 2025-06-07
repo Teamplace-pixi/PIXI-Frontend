a49fadba91f614e56c5d0562c2d81f5093ddb163
Minseo8170 <163138618+Minseo8170@users.noreply.github.com>
2025-06-07 20:42:30 +0900

    [Fix] 자잘하게 하나 더 수정

--- src/components/RepairSupportModal.jsx
+++ src/components/RepairSupportModal.jsx
@@ -17,9 +17,8 @@ export default function RepairSupportModal({
     const fetchApplyData = async () => {
       try {
         const res = await api.get(`/apply/apply_id=${applyId}`);
-        console.log('수리 지원 정보:', res.data);
-        if (!res.ok) throw new Error('수리 지원 정보를 불러오지 못했습니다.');
         const data = await res.json();
+        console.log('수리 지원 정보:', data);
         setApplyData(data);
       } catch (error) {
         console.error('수리 지원 정보 불러오기 실패:', error);
